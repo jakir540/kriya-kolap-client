bb37be55247ad05a676e1a920326c5ad046806e8
jakir540 <jakirhossainmd540@gamil.com>
2023-06-07 06:37:53 +0600

    navbar and footer added

---
+++ src/layouts/Main.jsx
@@ -0,0 +1,16 @@
+import Footer from "../pages/Shared/Footer/Footer";
+import Navbar from "../pages/Shared/Navbar/Navbar";
+import { Outlet } from "react-router-dom";
+
+const Main = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+            <Outlet></Outlet>
+            <Footer></Footer>
+         
+        </div>
+    );
+};
+
+export default Main;
